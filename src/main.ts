c62cd751da694b123d0f4bf7c7417d80bd3009c2
vinoth-Kumar-S <31274282+vinoth-Kumar-S@users.noreply.github.com>
2023-11-01 18:37:50 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
 
-registerLicense('ORg4AjUWIQA/Gnt2VlhhQlJCfV5DQmBWfFN0RnNYf1RxdF9CZEwgOX1dQl9gSH5TckRjWH1eeH1UTmg=');
+registerLicense('Enter your license');
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
